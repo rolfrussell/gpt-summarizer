5af7efaa5c8942dbd2c0a6b944c1e6ce04124286
Rolf Russell <rolf@tablexi.com>
2023-01-16 17:29:56 -0600

    clean up

--- gpt3_summarizer.py
+++ gpt3_summarizer.py
@@ -1,8 +1,17 @@
 import openai
 import os
-from time import time,sleep
-import textwrap
+import sys
+from datetime import datetime
 import re
+
+
+def load_conversation():
+    if len(sys.argv) < 2:
+        print("\nError:  Input file not specified")
+        print("Usage:  python " + sys.argv[0] + " [input file name]\n")
+        exit()
+    else:
+        return open_file(sys.argv[1])
 
 
 def open_file(filepath):
@@ -10,52 +19,59 @@ def open_file(filepath):
         return infile.read()
 
 
-openai.api_key = open_file('openaiapikey.txt')
+def save(chunks, summaries):
+    file_name = "logs/output_%s.txt" % datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    with open(file_name, 'w', encoding='utf-8') as outfile:
+        outfile.write("Chunks:\n\n")
+        outfile.write("\n------------------------------------\n".join(chunks))
+        outfile.write("\n\n------------------------------------------------------------------------\nSummaries:\n\n")
+        outfile.write("\n------------------------------------\n".join(summaries))
 
 
-def save_file(content, filepath):
-    with open(filepath, 'w', encoding='utf-8') as outfile:
-        outfile.write(content)
+def new_chunk():
+    return "Please summarize the following:\n"
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
-    max_retry = 5
-    retry = 0
-    while True:
-        try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
-                temperature=temp,
-                max_tokens=tokens,
-                top_p=top_p,
-                frequency_penalty=freq_pen,
-                presence_penalty=pres_pen,
-                stop=stop)
-            text = response['choices'][0]['text'].strip()
-            text = re.sub('\s+', ' ', text)
-            filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
-            return text
-        except Exception as oops:
-            retry += 1
-            if retry >= max_retry:
-                return "GPT3 error: %s" % oops
-            print('Error communicating with OpenAI:', oops)
-            sleep(1)
+def chunk(conversation, max_characters_in_chunk=8000):
+    chunks = list()
+    chunk = new_chunk()
+    paragraphs = re.split("Unknown Speaker  \d+:\d\d\n", conversation)
+    for paragraph in paragraphs:
+        if len(paragraph) >= max_characters_in_chunk:
+            print("\n\nERROR: Paragraph " + str(len(paragraph)) + " characters!!!\n")
+            print(paragraph)
+            exit()
+        elif len(chunk) + len(paragraph) <= max_characters_in_chunk:
+            chunk = chunk + "\n" + paragraph
+        else:
+            chunks.append(chunk)
+            chunk = new_chunk()
+    return chunks
+
+
+def summarize(chunks):
+    summaries = list()
+    print("Not calling CPT-3!!!!\n")
+    return summaries
+    for chunk in chunks:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=chunk,
+            temperature=0.2,
+            max_tokens=1000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        summary = response["choices"][0]["text"]
+        summaries.append(summary)
+        print(summary)
+    return summaries
 
 
 if __name__ == '__main__':
-    alltext = open_file('input.txt')
-    chunks = textwrap.wrap(alltext, 2000)
-    result = list()
-    count = 0
-    for chunk in chunks:
-        count = count + 1
-        prompt = open_file('prompt.txt').replace('<<SUMMARY>>', chunk)
-        prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-        summary = gpt3_completion(prompt)
-        print('\n\n\n', count, 'of', len(chunks), ' - ', summary)
-        result.append(summary)
-    save_file('\n\n'.join(result), 'output_%s.txt' % time())
+    openai.api_key = open_file('openaiapikey.txt')
+    conversation = load_conversation()
+    chunks = chunk(conversation)
+    summaries = summarize(chunks)
+    save(chunks, summaries)
